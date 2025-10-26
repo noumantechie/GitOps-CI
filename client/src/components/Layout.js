bdf2cb12aaf9a8cdc235e8f59f60785b702840c1
noumantechie <nomanyousaf712@gmail.com>
2025-10-26 21:11:19 +0500

    Modifying title

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -16,7 +16,7 @@ function Layout({ children }) {
           </div>
         </div>
       </header>
-      <AnimatedBanner message="Welcome to Nouman Yousaf 🚀" />
+      <AnimatedBanner message="Welcome to Nouman Yousaf and Kanzal Qalandri 🚀" />
       <div className="app-body">
         <aside className="sidebar slide-in-left">
           <h3>Connect</h3>
